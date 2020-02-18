add5d579edbcc90ae3a3f63eef4bad36bcf2d4ac
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-02-18 12:04:22 +0100

    Code cleanup and more trials with checkout_map

--- src/main.rs
+++ src/main.rs
@@ -18,23 +18,22 @@ fn checkout(items: Vec<&str>, price_map: HashMap<&str, u16>) -> u16 {
         offers_map.entry(item).or_insert(0);
 
         let apple_count = checkout_map.entry("A").or_insert(0);
-
+println!("Apple count: {}", apple_count);
         let banana_count = checkout_map.entry("B").or_insert(0);
-
-        println!("Apple count: {}, banana count: {}", apple_count, banana_count);
+println!("Banana count: {}", banana_count);
+        // println!("Apple count: {}, banana count: {}", apple_count, banana_count);
 
         if *apple_count % 3 == 0 && offers_map.contains_key("A") && *apple_count / 3 > offers_map["A"] {
             total -= 25;
             let current_apple_offer = offers_map.entry("A").or_insert(0);
             *current_apple_offer += 1;
-        } else if *banana_count % 3 == 0 && offers_map.contains_key("A") && *banana_count / 3 > offers_map["B"] {
+        } else if *banana_count % 3 == 0 && offers_map.contains_key("B") && *banana_count / 3 > offers_map["B"] {
             total -= 20;
+            let current_banana_offer = offers_map.entry("B").or_insert(0);
+            *current_banana_offer += 1;
         }
 
-        total += match price_map.get(item) {
-            Some(cost) => cost,
-            None => &0
-        };
+        total += price_map[item];
         println!("Found {} in basket", item);
     }
     // for (item, price) in &price_map {
@@ -42,13 +41,6 @@ fn checkout(items: Vec<&str>, price_map: HashMap<&str, u16>) -> u16 {
     // }
     total
 }
-
-// fn scan_item(item_code: &str, price_map: HashMap<&str, u16>) -> &u16 {
-//     match price_map.get(item_code) {
-//         Some(price) => price,
-//         None => &0
-//     }
-// }
 
 #[test]
 fn test_pos_spec_test_1 () {
