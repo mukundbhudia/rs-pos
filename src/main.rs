d2d33e3055d5ba09816e6bd3bac3b7f243f25a00
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-02-25 20:22:25 +0100

    Removed checkout function as it was too difficult to seperate out the borrow of the hash map

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
 #[allow(dead_code)]
 fn checkout(items: Vec<&str>, price_map: HashMap<&str, u16>) -> u16 {
-    let mut total: i16 = 0;
+    let mut total = 0;
     let mut checkout_map: HashMap<&str, u16> = HashMap::new();
     let mut offers_map: HashMap<&str, u16> = HashMap::new();
 
@@ -16,34 +16,22 @@ fn checkout(items: Vec<&str>, price_map: HashMap<&str, u16>) -> u16 {
 
         offers_map.entry(item).or_insert(0);
 
-        let offers_map2: HashMap<&str, u16> = offers_map.clone();
-
         let apple_count = checkout_map.get("A").unwrap_or(&0);
         let banana_count = checkout_map.get("B").unwrap_or(&0);
 
-        total += scan_item(item, &price_map) as i16;
-        let checked_offers = check_offers(apple_count, banana_count, offers_map2, &price_map);
-        total += checked_offers;
+        if apple_count % 3 == 0 && offers_map.contains_key("A") && apple_count / 3 > offers_map["A"] {
+            total -= price_map["A"];
+            let current_apple_offer = offers_map.entry("A").or_insert(0);
+            *current_apple_offer += 1;
+        } else if banana_count % 3 == 0 && offers_map.contains_key("B") && banana_count / 3 > offers_map["B"] {
+            total -= 20;
+            let current_banana_offer = offers_map.entry("B").or_insert(0);
+            *current_banana_offer += 1;
+        }
+
+        total += scan_item(item, &price_map);
     }
     total as u16
-}
-
-#[allow(dead_code)]
-fn check_offers(apple_count: &u16, banana_count: &u16, mut offers_map: HashMap<&str, u16>, price_map: &HashMap<&str, u16>) -> i16 {
-    let mut total: i16 = 0;
-    if apple_count % 3 == 0 && offers_map.contains_key("A") && apple_count / 3 > offers_map["A"] {
-        total -= 25;
-        let current_apple_offer = offers_map.entry("A").or_insert(0);
-        // println!("current_apple_offer: {}", current_apple_offer);
-        *current_apple_offer += 1;
-    } else if banana_count % 3 == 0 && offers_map.contains_key("B") && banana_count / 3 > offers_map["B"] {
-        total -= 20;
-        let current_banana_offer = offers_map.entry("B").or_insert(0);
-        // println!("current_banana_offer: {}", current_banana_offer);
-        *current_banana_offer += 1;
-    }
-    // println!("Check offers total: {}", total);
-    total
 }
 
 fn scan_item(item_code: &str, price_map: &HashMap<&str, u16>) -> u16 {
